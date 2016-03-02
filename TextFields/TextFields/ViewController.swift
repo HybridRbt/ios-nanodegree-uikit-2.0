5a417a9e0e225ea1bf29263fbc4baa5b2731cc87
Jianfeng Yang <jianfeng.yang0120@gmail.com>
2016-03-02 14:20:57 -0600

    add this new delegate to the viewController

--- TextFields/TextFields/ViewController.swift
+++ TextFields/TextFields/ViewController.swift
@@ -14,12 +14,14 @@ class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var textField1: UITextField!
     @IBOutlet weak var textField2: UITextField!
     @IBOutlet weak var textField3: UITextField!
+    @IBOutlet weak var textField4: UITextField!
+    
     @IBOutlet weak var characterCountLabel: UILabel!
     
     // Text Field Delegate objects
     let emojiDelegate = EmojiTextFieldDelegate()
     let colorizerDelegate = ColorizerTextFieldDelegate()
-    
+    let randomColorDelegate = RandomColorTextFieldDelegate()
     // Life Cycle Methods
     
     override func viewDidLoad() {
@@ -32,6 +34,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
         self.textField1.delegate = emojiDelegate
         self.textField2.delegate = colorizerDelegate
         self.textField3.delegate = self
+        self.textField4.delegate = randomColorDelegate
     }
 
     
